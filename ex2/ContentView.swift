6ced0da97440b5ef06aaf0b95e62070c54acf361
MichaelTseng <yucheng0939@gmail.com>
2020-12-08 01:52:52 +0800

    加說明1

--- ex2/ContentView.swift
+++ ex2/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 Text("Phone").border(Color.red)
              }.font(Font.system(size: 100,weight: .bold))
             
-            VStack(alignment:.leading){
+            VStack(alignment:.leading){    //maxHeight 最大高 度即是容器的高度也就是hstack的高度了,平均佔1/3高(靠左中)
                 Text("Jack").frame(maxHeight:.infinity).border(Color.red);               Text("jack@gmail.com").frame(maxHeight:.infinity).border(Color.red);          Text("4001234567").frame(maxHeight:.infinity).border(Color.red);
             }.font(Font.system(size: 25))
         }.fixedSize()           //就會平均分配高度(固定hstack為 高度去分配的
