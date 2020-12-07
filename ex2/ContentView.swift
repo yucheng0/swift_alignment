e4d74edd93f315b6bbcc2c9a99f75433149655b3
MichaelTseng <yucheng0939@gmail.com>
2020-12-08 01:17:05 +0800

    init

--- ex2/ContentView.swift
+++ ex2/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        /* vstack 有做好對齊力但 Hstack 卻沒有,加到最高度再限縮(fixedSize)*/
+        HStack{
+            VStack(alignment:.trailing){
+                Text("UserName").border(Color.red)
+                Text("Email").border(Color.red)
+                Text("Phone").border(Color.red)
+             }.font(Font.system(size: 35,weight: .bold))
+            
+            VStack(alignment:.leading){
+                Text("Jack").frame(maxHeight:.infinity).border(Color.red);               Text("jack@gmail.com").frame(maxHeight:.infinity).border(Color.red);          Text("4001234567").frame(maxHeight:.infinity).border(Color.red);
+            }.font(Font.system(size: 25))
+        }.fixedSize()           //就會平均分配高度
     }
 }
 
